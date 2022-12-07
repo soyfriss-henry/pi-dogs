e15d301400c5ef300e2e1d34cc7f8c99515ea095
soyfriss <federico.rissone@gmail.com>
2022-12-07 17:09:16 -0300

    Models naming convention (#4)
    * Add DogTemperament model
    
    * Change DB naming conventions

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,28 +3,34 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    ID: {
+  sequelize.define('Dog', {
+    id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
+      field: 'Id'
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      field: 'Name'
     },
     height: {
       type: DataTypes.STRING(50),
       allowNull: false,
+      field: 'Height'
     },
     weight: {
       type: DataTypes.STRING(50),
       allowNull: false,
+      field: 'Weight'
     },
     life_span: {
       type: DataTypes.STRING(50),
+      field: 'LifeSpan'
     }
   }, {
-    timestamps: false
+    timestamps: false,
+    freezeTableName: true
   });
 };
